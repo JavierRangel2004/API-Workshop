f38b9d2a8da7e99429bc74a2b1fcb8f8601f3514
Javier Rangel <0256158@up.edu.mx>
2023-05-16 14:35:42 -0600

    Pruebas
    xd

--- pruebas/chat.py
+++ pruebas/chat.py
@@ -7,31 +7,34 @@ class PokemonGo():
         self.url = 'https://pokeapi.co/api/v2/'
 
     def createtypesdata(self):
-        #if file exists dont do anything
         try:
-            df=pd.read_csv('tipos.csv')
+            pd.read_csv('tipos.csv')
             return
-        except:
+        except FileNotFoundError:
             pass
-        #if file doesnt exist, create it
-        response=requests.get(self.url+'type')
+
+        response = requests.get(self.url + 'type')
         self.log_requests(response)
+        
         if response.status_code == 200:
-            data=response.json()
+            data = response.json()
             tipos = []
+            
             for tipo in data['results']:
                 tipo_url = tipo['url']
                 tipo_response = requests.get(tipo_url)
                 self.log_requests(tipo_response)
                 tipo_data = tipo_response.json()
-                
+
                 debilidades = [d['name'] for d in tipo_data['damage_relations']['double_damage_from']]
                 resistencias = [r['name'] for r in tipo_data['damage_relations']['half_damage_from']]
-                ventaja =[v['name'] for v in tipo_data['damage_relations']['double_damage_to']]
+                ventaja = [v['name'] for v in tipo_data['damage_relations']['double_damage_to']]
+                
                 if tipo['name'] == "unknown" or tipo['name'] == "shadow":
                     continue
                 else:
                     tipos.append({'Tipo': tipo['name'], 'Debilidades': debilidades, 'Resistencias': resistencias, 'Ventaja': ventaja})
+                    
             df = pd.DataFrame(tipos)
             df.to_csv('tipos.csv', index=False)
         else:
@@ -45,68 +48,67 @@ class PokemonGo():
         url = self.url + 'pokemon/' + pokemon
         response = requests.get(url)
         self.log_requests(response)
+        
         if response.status_code == 200:
             return response.json()
         else:
             return None
         
     def get_pokemon_type(self, pokemon):
-
         url = self.url + 'pokemon/' + pokemon
         response = requests.get(url)
         self.log_requests(response)
+        
         if response.status_code == 200:
             return response.json()['types'][0]['type']['name']
         else:
             return None
         
-    def get_weakness (self, pokemon):
+    def get_pokemon_data(self, pokemon):
+        tipo = self.get_pokemon_type(pokemon)
+        df = pd.read_csv('tipos.csv')
+        df = df[df['Tipo'] == tipo]
+        return df
 
-        tipo=self.get_pokemon_type(pokemon)
-        df=pd.read_csv('tipos.csv')
-        df=df[df['Tipo']==tipo]
+    def get_weakness(self, pokemon):
+        df = self.get_pokemon_data(pokemon)
         return df['Debilidades'].values[0]
-    
-    def get_resistance (self, pokemon):
-        tipo=self.get_pokemon_type(pokemon)
-        df=pd.read_csv('tipos.csv')
-        df=df[df['Tipo']==tipo]
+
+    def get_resistance(self, pokemon):
+        df = self.get_pokemon_data(pokemon)
         return df['Resistencias'].values[0]
-   
-    def get_advantage (self, pokemon):
-        tipo=self.get_pokemon_type(pokemon)
-        df=pd.read_csv('tipos.csv')
-        df=df[df['Tipo']==tipo]
+
+    def get_advantage(self, pokemon):
+        df = self.get_pokemon_data(pokemon)
         return df['Ventaja'].values[0]
-    
-    def basic_info (self, pokemon):
-        print(f"{pokemon} es de tipo {self.get_pokemon_type(pokemon)}")
-        print(f"{pokemon} es resistente a {self.get_resistance(pokemon)}")
-        print(f"{pokemon} es debil contra {self.get_weakness(pokemon)}")
-        print(f"{pokemon} tiene ventaja contra {self.get_advantage(pokemon)}")
-    
-    def save_pokemon (self, pokemon):
-        #if file doesnt exist create it with header (nombre, tipo, ...)and add pokemon
-        #if file exists add new pokemon
+
+    def basic_info(self, pokemon):
+        tipo = self.get_pokemon_type(pokemon)
+        df = self.get_pokemon_data(pokemon)
+
+        print(f"{pokemon} es de tipo {tipo}")
+        print(f"{pokemon} es resistente a {df['Resistencias'].values[0]}")
+        print(f"{pokemon} es debil contra {df['Debilidades'].values[0]}")
+        print(f"{pokemon} tiene ventaja contra {df['Ventaja'].values[0]}")
+
+    def save_pokemon(self, pokemon):
+        df = pd.DataFrame(columns=['nombre', 'tipo', 'debilidad', 'resistencia', 'ventaja'])
         try:
             pd.read_csv('pokemons.csv')
-            data=[{'nombre': pokemon, 'tipo': self.get_pokemon_type(pokemon), 'debilidad': self.get_weakness(pokemon), 'resistencia': self.get_resistance(pokemon), 'ventaja': self.get_advantage(pokemon)}]
-            df=pd.DataFrame(data)
+            data = [{'nombre': pokemon, 'tipo': self.get_pokemon_type(pokemon), 'debilidad': self.get_weakness(pokemon), 'resistencia': self.get_resistance(pokemon), 'ventaja': self.get_advantage(pokemon)}]
+            df = pd.DataFrame(data)
             df.to_csv('pokemons.csv', mode='a', header=False, index=False)
-        except:
-            df=pd.DataFrame(columns=['nombre', 'tipo', 'debilidad', 'resistencia', 'ventaja'])
+        except FileNotFoundError:
+            df = pd.DataFrame(columns=['nombre', 'tipo', 'debilidad', 'resistencia', 'ventaja'])
             df.to_csv('pokemons.csv', index=False)
-            data=[{'nombre': pokemon, 'tipo': self.get_pokemon_type(pokemon), 'debilidad': self.get_weakness(pokemon), 'resistencia': self.get_resistance(pokemon), 'ventaja': self.get_advantage(pokemon)}]
-            df=pd.DataFrame(data)
+            data = [{'nombre': pokemon, 'tipo': self.get_pokemon_type(pokemon), 'debilidad': self.get_weakness(pokemon), 'resistencia': self.get_resistance(pokemon), 'ventaja': self.get_advantage(pokemon)}]
+            df = pd.DataFrame(data)
             df.to_csv('pokemons.csv', mode='a', header=False, index=False)
 
 
-
-
-Api= PokemonGo()
+Api = PokemonGo()
 
 Api.createtypesdata()
-
 Api.save_pokemon('charizard')
 Api.save_pokemon('pikachu')
 Api.save_pokemon('bulbasaur')
